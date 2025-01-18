1742a0de87c287306e218070e01d4c03aa75132b
Dineshsingh Rajpurohit <51024860+dineshraj92422@users.noreply.github.com>
2025-01-19 01:01:55 +0530

    Update show.route.js

--- routes/show.route.js
+++ routes/show.route.js
@@ -1,30 +1,18 @@
 const router = require('express').Router();
+const File = require('../models/file');
 
-
-const path = require('path')
-const File = require('../models/file')
-
-
-
-router.get('/:uuid',async(req,res)=>{
+router.get('/:uuid', async (req, res) => {
     try {
-        const file = await File.findOne({uuid:req.params.uuid});
-        if(!file){
-            return res.render('download',{error: 'Link went wrong'})
-        }
-        return res.render('download',
-        {
-            uuid:file.uuid,
-            fileName: file.filename,
-            fileSize:file.size,
-            downloadLink:`${process.env.APP_BASE_URL}/files/downloads/${file.uuid}`
-           
-        })
-
-    } catch (error) {
-        console.log(error.message)
-        return res.render('download',{error: 'something went wrong'})
+        const file = await File.findOne({ uuid: req.params.uuid });
+        // Link expired
+        if(!file) {
+            return res.render('download', { error: 'Link has been expired.'});
+        } 
+        return res.render('download', { uuid: file.uuid, fileName: file.filename, fileSize: file.size, downloadLink: `${process.env.APP_BASE_URL}/files/download/${file.uuid}` });
+    } catch(err) {
+        return res.render('download', { error: 'Something went wrong.'});
     }
-}); 
+});
+
 
 module.exports = router;
